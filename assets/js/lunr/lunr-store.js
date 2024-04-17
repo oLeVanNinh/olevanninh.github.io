09e208946bb57d2307af261da48309feb95779ed
Ninh <ninhlv.9984@gmail.com>
2024-04-17 19:25:23 +0700

    Deploy to GitHub Pages

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,55 @@
+var store = [{
+        "title": "Xây dựng hệ thống xử lý background bằng ruby",
+        "excerpt":"1. Lợi ích của việc sử dụng background job Đối với ứng dụng viết bằng Rails, mỗi khi có request đến, webserver tiếp nhận request và trả về resoponse, tại sao chúng ta cần sử dụng background? Bởi vì đối với những request cần phải tốn nhiều thời gian để...","categories": [],
+        "tags": ["ruby","background","work"],
+        "url": "/buiding-background-process-ruby/",
+        "teaser": null
+      },{
+        "title": "Vài ghi chép về replace function JS",
+        "excerpt":"Việc xử lý String là việc được thực hiện khá nhiều trong lập trình, một trong các thao tác thực hiện trong số đó là replace String. Trong ngôn ngữ Javascript, method được dùng để xử lý là replace, là một Standard built-in method của String. Tài liệu về cách...","categories": [],
+        "tags": ["javascript","replace","replace callback","work"],
+        "url": "/v%C3%A0i-ghi-ch%C3%A9p-v%E1%BB%81-replace-js/",
+        "teaser": null
+      },{
+        "title": "How To Build A Side Project",
+        "excerpt":"Why side project? As a developer, we always need to learn to catch up with the technology which changes every day. Sometimes we can feel overwhelmingly with it, or you can feel boring when the major job is maintaining the old system that uses the technology of 10 years ago....","categories": [],
+        "tags": ["random","work"],
+        "url": "/how-to-build-a-side-project/",
+        "teaser": null
+      },{
+        "title": "How to disable the browser back button in JS?",
+        "excerpt":"Introduction Back to the previous page is natively controlled by your browser, it’s not a good idea for the client site perform to prevent browser back, but sometimes you have a valid reason to do that. In this post, I will show you some ways to achieve this purpose, in...","categories": [],
+        "tags": ["javascript","work"],
+        "url": "/disable-browserback/",
+        "teaser": null
+      },{
+        "title": "Năm 2022 của tôi",
+        "excerpt":"Để tóm tắt năm 2022 của mình trong 1 bức hình thì có lẽ đây là bức hình mà mình thấy phù hợp nhất: 1.Nửa đầu năm bình yên Nếu bỏ qua chuyện VNIdex lập đỉnh 1500 điểm, Nga đánh Ukraine, rồi thì đồng yên rớt giá không phanh thì...","categories": [],
+        "tags": ["random"],
+        "url": "/my-2022/",
+        "teaser": null
+      },{
+        "title": "Thế giới của những ước muốn",
+        "excerpt":"Còn nhớ ngày này năm ngoái mình còn đang đi healing ở trên Sóc Sơn, lần ấy đi vậy mà cũng vật vã lắm. Mình cứ nghĩ là một đứa hướng nội “thuần chủng” như mình đi thì sẽ nhẹ nhàng cơ, ấy thế mà mấy ngày đầu cũng kiểu:...","categories": [],
+        "tags": ["random"],
+        "url": "/wishes/",
+        "teaser": null
+      },{
+        "title": 2023,
+        "excerpt":"Mình ngồi gõ những dòng này vào đúng 31-12, ngày cuối cùng của năm 2023. Mình nghĩ những ngày cuối năm luôn là thời điểm tốt để review lại những gì đã xảy ra trong năm. Thời gian gần đây mình bắt đồng không còn tin vào trí nhớ của...","categories": [],
+        "tags": ["random"],
+        "url": "/2023/",
+        "teaser": null
+      },{
+        "title": "Xây dựng cuộc sống hạnh phúc",
+        "excerpt":"Mình là người công giáo, mình vẫn nhớ như in câu giáo lí đầu tiên mình được học trước khi được xưng tội: Hỏi: Ta sống ở đời này để làm gì? Thưa: Ta sống ở đời này để tìm hạnh phúc, và hạnh phúc đích thực chỉ nơi Thiên...","categories": [],
+        "tags": ["random"],
+        "url": "/build-happy-life/",
+        "teaser": null
+      },{
+        "title": "Play with Cloud Formation",
+        "excerpt":"CloudFormation is a service provided by AWS to help users easily build, set up, and provision resources on AWS quickly, reducing the effort required for management. Using CloudFormation brings benefits such as: Using it as an Infrastructure as Code (IaC) tool, where all AWS resources are managed through templates, making...","categories": [],
+        "tags": ["work","aws","cloudformation"],
+        "url": "/getting-start-with-cf/",
+        "teaser": null
+      }]
